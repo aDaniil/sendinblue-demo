7d43e70494f0ac865f6c277195cd50dcd32282bb
Emrah Samdan <emrahsamdan@gmail.com>
2022-07-13 21:28:54 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,56 @@
+import { api, data, params,events } from "@serverless/cloud";
+import SibApiV3Sdk from  "sib-api-v3-sdk" ;
+
+var apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+var sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+var defaultClient = SibApiV3Sdk.ApiClient.instance;
+// Configure API key authorization: api-key
+var apiKey = defaultClient.authentications['api-key'];
+apiKey.apiKey = params.SENDINBLUE_API_KEY;
+
+api.post("/signup", async (req, res) => {
+  const {email, firstName, lastName} = req.body;
+
+  const result = data.set(`user:${email}`, {
+    email, 
+    firstName,
+    lastName
+  })
+
+  res.send({message: "user signed up!"});
+
+}); 
+
+data.on("created:user:*", async(event) => {
+  const record = event.item.value;
+  var contactsApiInstance = new SibApiV3Sdk.ContactsApi();
+  var createContact = new SibApiV3Sdk.CreateContact();
+  createContact.email = record.email; 
+  createContact.attributes = {
+    FIRSTNAME: record.firstName,
+    LASTNAME: record.lastName,
+  }; 
+  contactsApiInstance.createContact(createContact).then(function(data) {
+    console.log('API called successfully. Returned data: ' + JSON.stringify(data));
+  }, function(error) {
+    console.error(error);
+  });
+  await events.publish("contact.created", {after: "10 seconds"}, record); 
+});
+
+events.on("contact.created",async({body}) => {
+ var sendSmtpEmail = {
+  to: [{
+      email: body.email,
+      name: body.firstName
+  }],
+  templateId: 1, 
+};
+apiInstance.sendTransacEmail(sendSmtpEmail).then(function(data) {
+console.log('API called successfully. Returned data: ' + data);
+}, function(error) {
+console.error(error);
+});
+
+console.log ("welcome email sent!");
+});
